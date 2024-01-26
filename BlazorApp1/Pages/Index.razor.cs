a9b143c1892ad1c498373c5f0abc706a9ca54d87
kurowskipatryk <kurowski.patryk24@gmail.com>
2024-01-26 13:57:42 +0100

    Top bar Main layout ondragenter

---
+++ BlazorApp1/Pages/Index.razor.cs
@@ -0,0 +1,30 @@
+﻿using BlazorApp1.Data;
+using MudBlazor;
+
+namespace BlazorApp1.Pages
+{
+    public partial class Index
+    {
+        MudTabs tabs;
+
+        private List<User> usersList = new()
+       {
+           new User(){Name = "Adam", Surname="Kowalski", City="Radom", ImagePath="Images/Batman.png", Identifier="Empty"},
+           new User(){Name = "Piotr", Surname="Piotrkowski", City="Radom", ImagePath="Images/Rick.png", Identifier="Empty"},
+           new User(){Name = "Magda", Surname="Nowakowska", City="Warszawa", ImagePath="Images/Lena.png", Identifier="Empty"},
+           new User(){Name = "Jan", Surname="Nowak", City="Radom", ImagePath="Images/Boy.png", Identifier="Empty"},
+       };
+
+        private async Task ItemUpdated(MudItemDropInfo<User> item)
+        {
+            item.Item.Identifier = item.DropzoneIdentifier;
+        }
+
+        //private async Task OpenTab(int selectedIndex)
+        //{
+        //    //tabs.ActivatePanel(selectedIndex);
+
+
+        //}
+    }
+}
