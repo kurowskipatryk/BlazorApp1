729c36ee25bcb79f7569b4b95726a1152c9ed3cd
kurowskipatryk <kurowski.patryk24@gmail.com>
2024-01-26 15:59:14 +0100

    create component users

---
+++ BlazorApp1/Components/UserComponent.razor.cs
@@ -0,0 +1,11 @@
+﻿using BlazorApp1.Data;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp1.Components
+{
+    public partial class UserComponent
+    {
+        [Parameter]
+        public User userContext{ get; set; }
+    }
+}
